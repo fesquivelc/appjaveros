363ec616382654a8ce56ea80903a43a4926d6a9e
fesquivelc <fesquivelc@gmail.com>
2013-12-01 17:14:03 -0500

    Con base de datos incluida

--- market/models.py
+++ market/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Supermercado(models.Model):
+    nombre = models.CharField(max_length=150)
+    direccion = models.CharField(max_length=250)
+    telefono = models.CharField(max_length=10)
+    web = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=150)
+
+    def __unicode__(self):
+        return self.nombre
+

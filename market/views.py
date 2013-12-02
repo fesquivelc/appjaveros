6cfd1292e120e57d4fc34989871bac27ade5b568
fesquivelc <fesquivelc@gmail.com>
2013-12-02 17:07:18 -0500

    COSILLAS

--- market/views.py
+++ market/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from market.models import Producto
 
 
 def index(request):
     return render_to_response('index.html',{},context_instance=RequestContext(request))
 
 def prueba(request):
-    return render_to_response('principal.html',{},context_instance=RequestContext(request))
+    productos = Producto.objects.all()
+    return render_to_response('productos.html',{'productos':productos},context_instance=RequestContext(request))
+
